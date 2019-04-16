40a0410c11949ff1b68cc2f5a9c739f420ea5769
Adam Wujek <adam.wujek@cern.ch>
2019-04-16 10:26:31 +0200

    [port/board/ertm14] add i2c mapping
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- port/board/ertm14/i2c_mapping.h
+++ port/board/ertm14/i2c_mapping.h
@@ -15,55 +15,28 @@
 ///////////////////////
 
 enum {
-    CARRIER_TYPE_AFC = 0x01,
-    CARRIER_TYPE_AFCK,
-    CARRIER_TYPE_UNKNOWN = 0xFF
-};
-
-enum {
     I2C_BUS_UNKNOWN_ID = 0x00,
-    I2C_BUS_FMC1_ID,
-    I2C_BUS_FMC2_ID,
-    I2C_BUS_CPU_ID,
-    I2C_BUS_RTM_ID,
-    I2C_BUS_CLOCK_ID,
-    I2C_BUS_FPGA_ID
+    I2C_BUS_MAC_ID,
+    I2C_BUS_IPMI_ID,
+    I2C_BUS_TEMP_ID,
+    I2C_BUS_CNT
 };
 
 enum {
-    CHIP_ID_MUX = 0,
-    CHIP_ID_LM75AIM_0,
-    CHIP_ID_LM75AIM_1,
-    CHIP_ID_LM75AIM_2,
-    CHIP_ID_LM75AIM_3,
-    CHIP_ID_MAX6642,
-    CHIP_ID_RTC,
-    CHIP_ID_RTC_EEPROM,
-    CHIP_ID_EEPROM,
-    CHIP_ID_EEPROM_ID,
-    CHIP_ID_INA_0,
-    CHIP_ID_INA_1,
-    CHIP_ID_INA_2,
-    CHIP_ID_INA_3,
-    CHIP_ID_INA_4,
-    CHIP_ID_INA_5,
-    CHIP_ID_ADN,
-    CHIP_ID_SI57x,
-    CHIP_ID_FMC1_EEPROM,
-    CHIP_ID_FMC1_LM75_1,
-    CHIP_ID_FMC1_LM75_0,
-    CHIP_ID_FMC2_EEPROM,
-    CHIP_ID_FMC2_LM75_1,
-    CHIP_ID_FMC2_LM75_0,
-    CHIP_ID_RTM_PCA9554,
-    CHIP_ID_RTM_EEPROM,
-    CHIP_ID_RTM_LM75_0,
-    CHIP_ID_RTM_LM75_1
+    CHIP_ID_TMP100_FPGA,
+    CHIP_ID_TMP100_DCDC,
+    CHIP_ID_MAC,
+    I2C_CHIP_CNT
 };
+
+typedef enum {
+	I2C1_ID,			/**< ID I2C1 */
+	I2C2_ID,			/**< ID I2C2 */
+	I2C3_ID,			/**< ID I2C3 */
+	I2C_NUM_INTERFACE/**< Number of I2C interfaces in the chip */
+} I2C_ID_T ;
 
 #define I2C_MUX_CNT    2
-#define I2C_BUS_CNT    7
-#define I2C_CHIP_CNT   28
 
 i2c_mux_state_t i2c_mux[I2C_MUX_CNT];
 i2c_bus_mapping_t i2c_bus_map[I2C_BUS_CNT];
